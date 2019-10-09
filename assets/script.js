6a5e1c67763cb223ab064a6b159743c97b788fd1
Roslam24 <slametfaisal1@gmail.com>
2019-10-09 09:12:27 +0700

    Process 100

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+function kirimBoongan() {
+	alert("Terima kasih!\n Kami akan segera merespon pesan anda!");
+}
+function bersinar(x) {
+	x.style.backgroundColor = "#f39c12";
+}
+function normal(x) {
+	x.style.backgroundColor = "#ffffff";
+}
+function hover(x){
+	x.style.backgroundColor = "#f39c12";
+}
